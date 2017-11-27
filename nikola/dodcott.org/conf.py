3e99575382a4c97861ded81c9c552554606dad95
Ian Barton <ian@manor-farm.org>
2017-11-27 11:12:58 +0000

    Update author information.

--- nikola/dodcott.org/conf.py
+++ nikola/dodcott.org/conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Ian Barton"  # (translatable)
+BLOG_AUTHOR = "Dodcott-cum-Wilkesley Parish Council"  # (translatable)
 BLOG_TITLE = "Dodcott-cum-Wilkesley"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
