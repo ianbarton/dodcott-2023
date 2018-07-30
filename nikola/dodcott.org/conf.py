7f6555bb4679e78785dcfbb8c091b09c652ea4b7
Ian Barton <ian@manor-farm.org>
2018-07-30 17:57:03 +0100

    UNSLUGIFY_TITLES = True now deprecated. Comment out.

--- nikola/dodcott.org/conf.py
+++ nikola/dodcott.org/conf.py
@@ -1312,7 +1312,7 @@ BODY_END = """
 # If you hate "Filenames with Capital Letters and Spaces.md", you should
 # set this to true.
 FILE_METADATA_UNSLUGIFY_TITLES=True
-UNSLUGIFY_TITLES = True
+#UNSLUGIFY_TITLES = True
 
 # Additional metadata that is added to a post when creating a new_post
 # ADDITIONAL_METADATA = {}
